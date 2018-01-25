215b59aa37d16fc6a69529e0102cbbc0710a6045
Tiago <tmcrs123@gmail.com>
2018-01-25 16:39:45 +0000

    readme update

--- scripts/downloadHandler.js
+++ scripts/downloadHandler.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const copyFolderRecursive = require('./downloadHandler_copyFolder.js');
-const deleteFolderRecursive = require('./downloadHandler_deleteFolder.js');
+const fs = require("fs");
+const copyFolderRecursive = require("./downloadHandler_copyFolder.js");
+const deleteFolderRecursive = require("./downloadHandler_deleteFolder.js");
 
 const source = process.argv[2];
 const destination = process.argv[3];
@@ -12,38 +12,36 @@ let fileTypes;
 if (process.argv[5]) {
   fileTypes = process.argv.slice(5).map(filetype => `.${filetype}`);
 } else {
-  fileTypes = '*';
+  fileTypes = "*";
 }
-
-// progress bar
-// show usage and wait for input
 
 if (validInput(source, destination)) {
   deleteFolderRecursive.rootPath = source;
   copyFolderRecursive(source, destination, fileTypes);
-  if (keepSourceFolder === 'N')deleteFolderRecursive(source);
-  console.log('All done!');
+  if (keepSourceFolder === "N") deleteFolderRecursive(source);
+  console.log("All done!");
 } else {
   process.exit();
 }
 
 function validInput(source, destination) {
   if (!fs.existsSync(source)) {
-    process.on('exit', () => {
-      console.log('Oops! Files not copied. Source path is invalid.');
+    process.on("exit", () => {
+      console.log("Oops! Files not copied. Source path is invalid.");
     });
     return false;
   } else if (!fs.existsSync(destination)) {
-    process.on('exit', () => {
-      console.log('Oops! Files not copied. Destination path is invalid.');
+    process.on("exit", () => {
+      console.log("Oops! Files not copied. Destination path is invalid.");
     });
     return false;
-  } else if (!(keepSourceFolder === 'Y' || keepSourceFolder === 'N')) {
-    process.on('exit' , () => {
-        console.log('Please input a valid string (y/n) to signal if you want to keep source folder.');
-    })
+  } else if (!(keepSourceFolder === "Y" || keepSourceFolder === "N")) {
+    process.on("exit", () => {
+      console.log(
+        "Please input a valid string (y/n) to signal if you want to keep source folder."
+      );
+    });
     return false;
   }
   return true;
 }
-
